c98c83537b75a3c4a8a7647b127cc9ffc73865d2
sailendrachettri <sailendra9083@gmail.com>
2022-07-13 08:35:53 +0530

    bubble sort optimized

--- algorithms_/bubble__sort__.cpp
+++ algorithms_/bubble__sort__.cpp
@@ -13,11 +13,16 @@ void printArr(int arr[], int size){
 
 void bubbleSort(int arr[], int size){
     for(int i = 0; i < size-1; i++){
+        bool swapped = false;
+        // cout<<"step "<<i+1<<endl;
         for(int j = 0; j < size-i; j++){
             if(arr[j]>arr[j+1]){
                 swap(arr[j], arr[j+1]);
+                swapped = true;
             }
         }
+
+        if(swapped==false) break;
     }
 }
 
