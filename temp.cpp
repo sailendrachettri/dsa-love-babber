c1d510b6519591b5a6ba524592bb5ee07cae51c6
sailendrachettri <sailendra9083@gmail.com>
2022-07-14 06:18:37 +0530

    stl

--- temp.cpp
+++ temp.cpp
@@ -1,12 +1,12 @@
-// void printArr(int arr[], int size){
-//     for(int i = 0; i < size; i++){
-//         cout<<arr[i]<<" ";
-//     }cout<<endl;
-// }
+void printArr(int arr[], int size){
+    for(int i = 0; i < size; i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+}
 
-//     int arr[100];
-//     int size; cin>>size;
-//     for(int i = 0; i < size; i++){
-//         cin>>arr[i];
-//     }
-//     printArr(arr, size);
+    int arr[100];
+    int size; cin>>size;
+    for(int i = 0; i < size; i++){
+        cin>>arr[i];
+    }
+    printArr(arr, size);
