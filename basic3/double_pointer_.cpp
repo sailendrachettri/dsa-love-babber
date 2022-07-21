f87a1e76221718ac4e6c5201599888992e77b92a
sailendrachettri <sailendra9083@gmail.com>
2022-07-21 11:39:13 +0530

    double pointer

--- basic3/double_pointer_.cpp
+++ basic3/double_pointer_.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <bits/stdc++.h>
-
 using namespace std;
+
+
+void update(int **ptr){
+    ptr = ptr + 1;
+    // (**ptr) = (**ptr) + 1;
+}
 
 int main(){
     freopen("input.txt", "r", stdin);
@@ -18,11 +23,17 @@ int main(){
     cout<<"ptr2: "<<ptr2<<endl;
     cout<<"*ptr2: "<<*ptr2<<endl;
     cout<<"**ptr2: "<<**ptr2<<endl;
-*/
+
 
     cout<< num <<" "<< (*ptr) <<" "<< (**ptr2)<<endl;
     **ptr2 += 10;
     cout<< num <<" "<< (*ptr) <<" "<< (**ptr2)<<endl;
+
+*/
+
+    cout<<num<<endl;
+    update(ptr2);
+    cout<<num<<endl;
 
     return 0;
 }
