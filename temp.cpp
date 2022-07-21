3a19c93cc38323678ee242b580a99f9d6de2b59a
sailendrachettri <sailendra9083@gmail.com>
2022-07-21 08:15:27 +0530

    pointer function

--- temp.cpp
+++ temp.cpp
@@ -1,3 +1,23 @@
+/* Date:  - 07 - 2022
+Author: Sailendra */
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    cout<<"Let's get started"<<endl;
+
+    return 0;
+}
+
+
+/*
+
 void printArr(int arr[], int size){
     for(int i = 0; i < size; i++){
         cout<<arr[i]<<" ";
@@ -10,3 +30,5 @@ void printArr(int arr[], int size){
         cin>>arr[i];
     }
     printArr(arr, size);
+
+/
