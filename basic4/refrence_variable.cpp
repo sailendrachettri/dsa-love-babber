444c036091436f2424f8aaaa7586e89c99c96a1c
sailendrachettri <sailendra9083@gmail.com>
2022-07-21 18:26:35 +0530

    dynamic memory for array

--- basic4/refrence_variable.cpp
+++ basic4/refrence_variable.cpp
@@ -5,11 +5,16 @@ Author: Sailendra */
 #include <bits/stdc++.h>
 using namespace std;
 
+void update(int &n){
+    n++;
+}
+
 int main()
 {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 
+/*
     int num = 101;
     int &num2 = num;
 
@@ -17,6 +22,13 @@ int main()
     // num++; 
     num2 += 101;
     cout<<num<<" "<<num2<<endl;
+*/
+
+    int num = 4;
+    cout << num << endl;
+    update(num);
+    cout << num << endl;
+
 
     return 0;
 }
